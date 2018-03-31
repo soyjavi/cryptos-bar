d026e0c058c2998dd033c964a8b6df22cf9ea399
@soyjavi <javi.jimenez.villar@gmail.com>
2018-03-31 14:18:40 +0700

    Use only one instance of browser and tray
    Next steps use singleton within a React component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Tray } from 'electron';
 import path from 'path';
 import url from 'url';
 
 import { C } from './common';
+
+const { STYLE: { MAIN_WINDOW } } = C;
+
+let mainWindow;
+let tray;
 
 app.setName(C.APP_NAME);
 app.dock.hide();
 
 app.on('ready', () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    // backgroundColor: 'transparent',
+  mainWindow = new BrowserWindow({
     frame: false,
-    height: C.MAIN_WINDOW.MIN_HEIGHT,
-    width: C.MAIN_WINDOW.MIN_WIDTH,
+    height: MAIN_WINDOW.HEIGHT,
+    width: MAIN_WINDOW.WIDTH,
     show: false,
     transparent: true,
     vibrancy: 'ultra-dark',
@@ -25,8 +29,38 @@ app.on('ready', () => {
     slashes: true,
   }));
 
+  // Create tray
+  tray = new Tray(path.resolve(process.cwd(), 'public', 'assets', 'trayIcon.png'));
+  tray.setTitle('Wait a moment...');
+
+  tray.on('click', () => {
+    if (mainWindow.isVisible()) {
+      tray.setHighlightMode('never');
+      mainWindow.hide();
+    } else {
+      const { x, y } = tray.getBounds();
+      tray.setHighlightMode('always');
+      mainWindow.setPosition(x, y);
+      mainWindow.show();
+    }
+  });
+
+  app.on('browser-window-blur', () => {
+    tray.setHighlightMode('never');
+    mainWindow.hide();
+  });
+
   // Open the DevTools.
   mainWindow.webContents.openDevTools({ mode: 'detach' });
 
-  global.shared = { mainWindow };
+  global.shared = {
+    mainWindow,
+    tray,
+  };
 });
+
+app.on('browser-window-blur', () => {
+  tray.setHighlightMode('never');
+  mainWindow.hide();
+});
+
