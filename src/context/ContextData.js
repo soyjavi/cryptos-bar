a5c2cc7584851e1ac17cd9c3d3060548aa5d6ab7
@soyjavi <javi.jimenez.villar@gmail.com>
2018-03-31 14:17:36 +0700

    Context data fetch the TOP20 currencies using CoinMarketCap service

--- src/context/ContextData.js
+++ src/context/ContextData.js
@@ -1,13 +1,23 @@
 import { node } from 'prop-types';
 import React, { PureComponent, createContext } from 'react';
 
+import { fetch } from '../common';
+
 const Context = createContext();
 const { Provider, Consumer: ConsumerData } = Context;
 
 class ProviderData extends PureComponent {
   state = {
-    dataSource: ['BTC', 'ETH', 'LTC', 'XMR', 'EOS'],
-    favorites: ['BTC', 'ETH', 'LTC'],
+    coinList: undefined,
+    favorites: ['BTC', 'ETH', 'LTC', 'XMR'],
+  }
+
+  async componentDidMount() {
+    const coinList = await fetch('https://api.coinmarketcap.com/v1/ticker/?limit=20') || undefined;
+
+    this.setState({
+      coinList,
+    });
   }
 
   render() {
